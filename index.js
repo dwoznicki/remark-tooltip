3e39594615a9b4c3c334020fb1e3a31c848831e4
Daniel Woznicki <danwoz@docspot.com>
2020-05-22 17:51:18 -0700

    Changed tooltip syntax to something a bit less common Removed old cruft from AST node

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const unified = require("unified");
 const remarkParse = require("remark-parse");
 
-const TOOLTIP_REGEX = /^&\[(.+?)\]\((.+?)\)/;
+const TOOLTIP_REGEX = /^\[(.+?)\]\^(.+?)\^/;
 
 function parse(doc) {
     return unified()
@@ -11,7 +11,7 @@ function parse(doc) {
 
 function attacher() {
     function locator(value, fromIndex) {
-        return value.indexOf("&[", fromIndex);
+        return value.indexOf("[", fromIndex);
     }
 
     function inlineTokenizer(eat, value) {
@@ -24,16 +24,7 @@ function attacher() {
             return eat(match[0])({
                 type: "tooltip",
                 children: [
-                    {
-                        type: "tooltip",
-                        children: parse(base).children[0].children,
-                        data: {
-                            hName: "span",
-                            hProperties: {
-                                class: "tooltip-base",
-                            }
-                        }
-                    },
+                    { type: "text", value: base },
                     {
                         type: "tooltip",
                         children: parse(tooltip).children[0].children,
@@ -46,28 +37,11 @@ function attacher() {
                     }
                 ],
                 data: {
-                    hName: "div",
+                    hName: "span",
                     hProperties: {
                         class: "tooltip",
                     }
                 }
-                /*
-                type: "tooltip",
-                base,
-                tooltipRaw: tooltip,
-                tooltip: parse(tooltip).children,
-                // For rehype
-                children: [
-                    { type: "text", value: baseText }
-                ],
-                data: {
-                    hName: "span",
-                    hProperties: {
-                        class: "tooltip-base",
-                        //"data-tooltip": tooltipContent,
-                    }
-                }
-                */
             });
         }
     }
